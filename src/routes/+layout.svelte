74fd95a3ec6126e114a60c55254181102113a436
Amy J. Ko <ajko@uw.edu>
2023-02-04 10:50:28 -0800

    Fixed edition loading.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
     import { Edition } from 'bookish-press';
     import { default as EditionModel } from 'bookish-press/models/book/Edition';
-    import book from '$lib/assets/book.json';
+    import spec from '$lib/assets/edition.json';
     import { writable } from 'svelte/store';
     import { setContext } from 'svelte';
     import { EDITION } from 'bookish-press';
 
-    const edition = EditionModel.fromJSON(undefined, book);
+    const edition = EditionModel.fromJSON(undefined, spec);
 
     setContext(EDITION, writable<EditionModel>(edition));
 </script>
 
-<Edition {edition} editable={false}>
+<Edition {edition}>
     <slot />
 </Edition>
