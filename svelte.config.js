4fbe80eb475d4c0d0d284ef16d5dadce1da95031
Amy J. Ko <ajko@uw.edu>
2023-02-04 08:06:03 -0800

    Convert to pure static site generation.

--- svelte.config.js
+++ svelte.config.js
@@ -3,15 +3,13 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter({
-			fallback: "index.html"
-		})
-	}
+    kit: {
+        adapter: adapter(),
+    },
 };
 
 export default config;
