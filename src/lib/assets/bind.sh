24587c995eac82ca63846677137a7dde26c74b40
Amy J. Ko <ajko@uw.edu>
2023-02-05 20:38:12 -0800

    Moved bind script to reader for reuse.

---
+++ src/lib/assets/bind.sh
@@ -0,0 +1,14 @@
+# Clean up prior build
+rm -r build
+# Get into this repo
+cd bookish-reader
+# Install dependencies
+npm install
+# Run the bind script on the book in the parent directory
+npm run bind ../book.json
+# Copy the build into the book's directory
+cp -r build ../build
+# 
+cd ..
+# Remove the reader
+rm -rf bookish-reader
