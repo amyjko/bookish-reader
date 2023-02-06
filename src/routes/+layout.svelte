a75b1c3a38b66fcb34c0d592510c6adbcb53281e
Amy J. Ko <ajko@uw.edu>
2023-02-05 16:10:47 -0800

    Fixed base in reader.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,9 +14,16 @@
         spec as unknown as EditionSpecification
     );
 
+    let base = edition.base ?? '';
+    // Strip trailing slash if provided.
+    if (base.length > 0 && base.endsWith('/'))
+        base = base.substring(0, base.length - 1);
+    // Prepend slash if not provided.
+    if (base.length > 0 && base.charAt(0) !== '/') base = '/' + base;
+
     setContext(EDITION, writable<EditionModel>(edition));
 </script>
 
-<Edition {edition} base={edition.base ? `${edition.base}/` : ''}>
+<Edition {edition} {base}>
     <slot />
 </Edition>
