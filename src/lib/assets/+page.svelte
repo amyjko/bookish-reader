6247b10a2db9f721ed3cea7ba285426d000c52e0
Amy J. Ko <ajko@uw.edu>
2023-02-05 17:05:45 -0800

    Fixed chapter ID resolution.

--- src/lib/assets/+page.svelte
+++ src/lib/assets/+page.svelte
@@ -5,7 +5,7 @@
 
     const edition = getEdition();
     const path = $page.route.id;
-    // Strip the / from the front of the path.
+    // Strip the / from the front and end of the the path.
     const chapterID = path ? path.substring(1) : null;
     const chapter =
         $edition !== undefined && chapterID !== null
@@ -16,7 +16,7 @@
 {#if chapter}
     <Chapter {chapter} />
 {:else if $edition !== undefined}
-    Unable to find chapter {$page.params.chapterid}.
+    Unable to find chapter {chapterID}.
 {:else}
     Chapter is not inside an edition.
 {/if}
