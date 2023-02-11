cbb8eee07c51befc154d1376d0ca1e379ec450db
Amy J. Ko <ajko@uw.edu>
2023-02-11 11:43:06 -0800

    Handle .html on routes.

--- src/routes/[chapterid]/+page.svelte
+++ src/routes/[chapterid]/+page.svelte
@@ -3,12 +3,20 @@
     import Chapter from 'bookish-press/components/page/Chapter.svelte';
     import { Unknown } from 'bookish-press';
     import { page } from '$app/stores';
+    import { getEdition } from 'bookish-press/components/page/Contexts';
 
-    export let data: { chapter: ChapterModel | undefined };
+    export let data: { chapter: ChapterModel | undefined } | undefined;
+
+    let edition = getEdition();
+
+    // Resolve the chapter either with the server side data or the edition context if that doesn't exist.
+    let chapter =
+        data?.chapter ??
+        $edition?.getChapter($page.params.chapterid.replace('.html', ''));
 </script>
 
-{#if data.chapter}
-    <Chapter chapter={data.chapter} />
+{#if chapter}
+    <Chapter {chapter} />
 {:else}
     <Unknown
         >No chapter by the name <code>{$page.params.chapterid}</code> exists.</Unknown
